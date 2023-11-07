2d3f6f6d24138ff955a520f84cb2799424bfe36d
Nainoa Dinson <nd808@msn.com>
2023-11-06 18:39:29 -1000

    created an array of questions for user input

--- index.js
+++ index.js
@@ -3,7 +3,44 @@ const inquirer = require('inquirer');
 const fs = require('fs')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'Title',
+    message: 'What is the title of your project?'
+  },
+  {
+    type: 'input',
+    name: 'Description',
+    message: 'Provide a short description explaining the what, why, and how of your application.'
+  },
+  {
+    type: 'input',
+    name: 'Installation',
+    message: 'What are the steps required to install your application?'
+  },
+  {
+    type: 'input',
+    name: 'Usage',
+    message: 'Provide instructions and examples for use.'
+  },
+  {
+    type: 'input',
+    name: 'Contributing',
+    message: 'List your collaborators.'
+  },
+  {
+    type: 'input',
+    name: 'Tests',
+    message: 'Include tests for your aapplication and provide examples on how to run them.'
+  },
+  {
+    type: 'list',
+    name: 'License',
+    message: 'Choose a license for your application.',
+    choices: ['MIT']
+  }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
