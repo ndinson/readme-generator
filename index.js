a8ed58e4c9b5a1c30855548a5d776aabf06e83cf
Nainoa Dinson <nd808@msn.com>
2023-11-07 02:59:56 -1000

    added gitHUb username and email to the questions array

--- index.js
+++ index.js
@@ -1,44 +1,55 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
 const questions = [
   {
     type: 'input',
     name: 'Title',
-    message: 'What is the title of your project?'
+    message: 'What is your gitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'Title',
+    message: 'What is your email address?',
+  },
+  {
+    type: 'input',
+    name: 'Title',
+    message: 'What is the title of your project?',
   },
   {
     type: 'input',
     name: 'Description',
-    message: 'Provide a short description explaining the what, why, and how of your application.'
+    message: 'Provide a short description explaining the what, why, and how of your application.',
   },
   {
     type: 'input',
     name: 'Installation',
-    message: 'What are the steps required to install your application?'
+    message: 'What are the steps required to install your application?',
   },
   {
     type: 'input',
     name: 'Usage',
-    message: 'Provide instructions and examples for use.'
+    message: 'Provide instructions and examples for use.',
   },
   {
     type: 'input',
     name: 'Contributing',
-    message: 'List your collaborators.'
+    message: 'List your collaborators.',
   },
   {
     type: 'input',
     name: 'Tests',
-    message: 'Include tests for your aapplication and provide examples on how to run them.'
+    message: 'Include tests for your aapplication and provide examples on how to run them.',
   },
   {
     type: 'list',
     name: 'License',
     message: 'Choose a license for your application.',
-    choices: ['MIT']
+    choices: ['MIT', 'APACHE-2.0', 'GPL-3.0', 'BSD3', 'None'],
   }
 ];
 
@@ -51,7 +62,9 @@ function writeToFile(fileName, data) {}
 function init() {
   inquirer.prompt(questions)
     .then(res => {
-    console.log(res)
+    //console.log(res)
+    const template = generateMarkdown()
+    //console.log(template)
 })
 }
 
